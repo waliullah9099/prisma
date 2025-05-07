e0de2ac479fec60aa17bccfa9b180e09657abbba
MD Waliullah <waliullah9099@gmail.com>
2025-05-07 12:23:50 +0600

    relational filters

--- src/relational-query.ts
+++ src/relational-query.ts
@@ -3,13 +3,42 @@ import { PrismaClient } from '@prisma/client';
 const p = new PrismaClient();
 
 const main = async () => {
-    const result = await p.user.findUnique({
-        where: {
-            id: 1
-        }
-    }).post() // fluent API in prisma 
+    // const result = await p.user.findUnique({
+        // where: {
+            // id: 1
+        // }, 
+        // includes as like populate in mongoose 
+        // include: {
+        //     post: true 
+        // }
+    // }).profile() // fluent API in prisma 
 
-    console.log(result);
+
+    // const result = await p.user.findUnique({
+        // where: {
+            // id: 1
+        // }, 
+        // includes as like populate in mongoose 
+        // include: {
+            // post: true 
+        // }
+    // })
+
+
+    // relational filters 
+    const result = await p.user.findMany({
+        include: {
+            post: {
+                where: {
+                    published: true
+                }
+            }
+        }
+    })
+
+
+
+    console.dir(result, {depth: Infinity});
     
 }
 
